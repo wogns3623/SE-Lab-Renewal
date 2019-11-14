8850ecadbfda6b8c1f62fbd9e8ff372b2da5aeb5
Unknown <wogns3623@gmail.com>
2019-11-15 00:40:58 +0900

    Add ErrorContent component
    Add ErrorContent component for missing main content

---
+++ src/component/ErrorContent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class PageTemplate extends Component {
+  render() {
+    return (
+      <div className="ErrorContent">
+        <h2>OOPS!</h2>
+        <p>
+          This page's content is not ready yet :(<br></br>
+          Sorry!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default PageTemplate;
