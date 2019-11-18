0023512e34486c0f7164d6b41a5adc4315f42526
K.Seungho <rhanfwlq980@hanyang.ac.kr>
2019-11-18 22:24:29 +0900

    Home page in operation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../style/Home.scss';
 
+
 import PageTamplate from 'component/template/PageTemplate.js';
 
 class Home extends Component {
@@ -8,7 +9,16 @@ class Home extends Component {
     return (
       <PageTamplate className='Home'>
         <div className='body'>
-          It's my home town !
+          <div className='container'>
+            <h1><img src="https://img.icons8.com/ios-filled/100/000000/google-scholar.png" alt="icon"/>SELab</h1>
+            <p>Welcome to <span>S</span>oftware <span>E</span>ngineering <span>Lab</span>oratory !</p>
+            <p>I LOVE <span>Scott Uk-Jin Lee</span> professor.</p>
+            <p>We Service <span>Notice, Members, Courses, Login</span>, etc.</p>
+            <p>Please give me a <span>A+</span>!</p>
+          </div>
+          <div className="row">
+            asd
+          </div>
         </div>
       </PageTamplate>
     );
