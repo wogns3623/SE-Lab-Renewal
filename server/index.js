8463065fe8d7d7d2c11ba31068273134b8f14f2d
wogns3623 <wogns3623@gmail.com>
2019-12-20 00:58:51 +0900

    change dir

--- server/index.js
+++ server/index.js
@@ -8,16 +8,17 @@ const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 // app.use('/api', api);
-app.use(
-  proxy("/api", {
-    target: `http://ilovescott.kro.kr/`
-  })
-);
-app.use(
-  proxy("/assets", {
-    target: `http://ilovescott.kro.kr/`
-  })
-);
-app.use("/assets", express.static(path.join(__dirname, "build/assets")));
+// app.use(
+//   proxy("/api", {
+//     target: `http://ilovescott.kro.kr/`
+//   })
+// );
+// app.use(
+//   proxy("/assets", {
+//     target: `http://ilovescott.kro.kr/`
+//   })
+// );
+app.use("/assets", express.static(path.join(__dirname, "../assets")));
+app.use("/", express.static(path.join(__dirname, "../build")));
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
