979eb0d4183179c09dad2edfaa991def66fc8e3d
wogns3623 <wogns3623@gmail.com>
2019-12-20 00:02:25 +0900

    change port

--- server/index.js
+++ server/index.js
@@ -4,20 +4,20 @@ const proxy = require("http-proxy-middleware");
 const path = require("path");
 const app = express();
 // const api = require('./routes/index');
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 // app.use('/api', api);
 app.use(
   proxy("/api", {
-    target: "http://localhost:3001/"
+    target: `http://ilovescott.kro.kr/`
   })
 );
 app.use(
   proxy("/assets", {
-    target: "http://localhost:3001/"
+    target: `http://ilovescott.kro.kr/`
   })
 );
 app.use("/assets", express.static(path.join(__dirname, "build/assets")));
 
-const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
