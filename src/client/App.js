269ce945c8df797a92834ed9f9ac3a4b502f7895
wogns3623 <wogns3623@gmail.com>
2020-12-04 06:40:38 +0900

    Remove some devserver warnings

--- src/client/App.js
+++ src/client/App.js
@@ -1,34 +1,22 @@
 import React, { Component } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-import {
-  Home,
-  Notice,
-  Members,
-  Research,
-  Publications,
-  Courses,
-  Gallery,
-  Slide,
-  Test,
-  NoMatch
-} from "pages";
+import * as pages from "pages";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/notice" component={Notice} />
-          <Route path="/members" component={Members} />
-          <Route path="/research" component={Research} />
-          <Route path="/publications" component={Publications} />
-          <Route path="/courses" component={Courses} />
-          <Route path="/gallery" component={Gallery} />
-          <Route path="/slide" component={Slide} />
-          <Route path="/test" component={Test} />
-          <Route component={NoMatch} />
+          <Route exact path="/" component={pages.Home} />
+          <Route path="/notice" component={pages.Notice} />
+          <Route path="/members" component={pages.Members} />
+          <Route path="/research" component={pages.Research} />
+          <Route path="/publications" component={pages.Publications} />
+          <Route path="/courses" component={pages.Courses} />
+          <Route path="/gallery" component={pages.Gallery} />
+          <Route path="/slide" component={pages.Slide} />
+          <Route component={pages.NoMatch} />
         </Switch>
       </BrowserRouter>
     );
