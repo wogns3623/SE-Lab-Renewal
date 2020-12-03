b21156dc64960e3b08e09906a0502899244aee4f
wogns3623 <wogns3623@gmail.com>
2020-12-04 07:51:54 +0900

    Refactor header's css using scss syntax
    Scss문법에 맞춰 헤더 관련 파일을 리팩토링함

--- src/component/common/header/Logo.js
+++ src/component/common/header/Logo.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import "style/Logo.scss";
+import "style/Header/Logo.scss";
 import logoImg from "assets/images/selab_logo_S.png";
 
 class Header extends Component {
   render() {
     return (
       <div className="Logo pull-left">
-        <Link exact={true} to="/">
+        <Link exact="true" to="/">
           <img src={logoImg} alt="SE Lab logo"></img>
         </Link>
       </div>
